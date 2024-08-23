900783e3175132e561f9209679928e0d754c05fa
Sasha191312 <alex.perednya20@gmail.com>
2024-08-23 16:22:32 +0300

    logout button

--- src/Home.js
+++ src/Home.js
@@ -41,21 +41,22 @@ const Home = () => {
 
   return (
     <div>
-      <button onClick={handleLogout} style={{ position: 'absolute', top: '10px', left: '10px' }}>
-        Log Out
-      </button>
       <h2>Welcome to the Secure File Storage System</h2>
       {user ? (
         <div>
           <p>You are logged in as {user.email}</p>
           <button onClick={handleUpload}>Upload File</button>
           <button onClick={handleManageFiles}>Manage Files</button>
+          <button onClick={handleLogout}>
+        Log Out
+      </button>
         </div>
       ) : (
         <div>
           <p>Please log in or sign up to continue.</p>
           <button onClick={() => navigate('/login')}>Log In</button>
           <button onClick={() => navigate('/signup')}>Sign Up</button>
+         
         </div>
       )}
     </div>
